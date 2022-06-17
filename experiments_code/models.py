c789ba69a43cc099efa1113d74d503324b4b95a3
Asiegbu Kanu-Asiegbu <akanu@wailord.engin.umich.edu>
2022-06-17 18:21:03 -0400

    lstm training

--- experiments_code/models.py
+++ experiments_code/models.py
@@ -2,14 +2,17 @@ import os, sys, time
 import tensorflow as tf
 from tensorflow import keras
 
-from config import hyparams, exp
+from config import hyparams, exp, loc
 from tensorflow.keras.layers import Lambda
+from data import data_lstm, tensorify
 import wandb 
 from wandb.keras import WandbCallback
+from load_data import norm_train_max_min
+from sklearn.model_selection import train_test_split
 
 from os.path import join
 from custom_functions.utils import make_dir
-
+from metrics_plot import loss_plot
 
 # To Do List
 ## Import time and make
@@ -135,131 +138,13 @@ def custom_loss(weight_ratio):
     return loss
 
 
-def binary_network(train_bm, val_bm, model_loc, nc,
-                   weighted_binary, weight_ratio, output_bias, run,
-                   epochs=300, save_model=True):
-    """
-    train_bm: train_data_tensor
-    val_bm: validation_data tensor
-    model_loc : location to save models too
-    nc: naming convention. list that contains [model, type,dataset,seq, abnormal_split]
-        model: Dense_5_Drop_2
-        type: xywh, tlbr
-        dataset: ped1,ped2,st, avenue
-        seq: size of sequence, 20, 5 etc int of sequence
-        abnormal_split: percentage of abnormal frames to put in test frame
+if __name__ == '__main__':
+    traindict, testdict = data_lstm(    loc['data_load'][exp['data']]['train_file'],
+                                loc['data_load'][exp['data']]['test_file'],
+                                hyparams['input_seq'], hyparams['pred_seq']
+                                , 
+                                )
 
-    weighted_binary: weighted_binary loss function,
-    weight_ratio: input into the weighted loss function len(pos)/len(neg)
-    output_bias: if not set output bias is set to 0 later in function
-        """
-
-
-    # delete if using wandb
-    
-    class my_struct:
-        pass
-
-    if not hyparams['networks']['binary_classifier']['wandb']:
-        config = my_struct()
-    else:
-        config = run.config
-
-
-    config.neurons = hyparams['networks']['binary_classifier']['neurons']
-    config.dropout_ratio = hyparams['networks']['binary_classifier']['dropout']
-    config.lr = hyparams['networks']['binary_classifier']['lr']
-    config.seed = hyparams['networks']['binary_classifier']['seed']
-    config.abnormal_split = hyparams['networks']['binary_classifier']['abnormal_split']
-    config.val_ratio = hyparams['networks']['binary_classifier']['val_ratio']
-    config.batch_size = hyparams['networks']['binary_classifier']['batch_size']
-    config.data = exp['data']
-    if exp['1']:
-        config.exp_type = '1'
-    elif exp['2']:
-        config.exp_type = '2'
-    elif exp['3_1']:
-        config.exp_type ='3_1'
-    elif exp['3_2']:
-        config.exp_type = '3_2'
-
-    # gpu = '/device:GPU:'+gpu
-    with tf.device('/device:GPU:0'):
-
-        # create model
-        if output_bias is not None:
-            output_bias = keras.initializers.Constant(output_bias)
-
-        model = keras.Sequential()
-        model.add(keras.layers.Dense(config.neurons, input_dim=1, activation='relu'))
-        model.add(keras.layers.Dense(config.neurons, activation='relu'))
-        model.add(keras.layers.Dropout(config.dropout_ratio))  # comment
-        model.add(keras.layers.Dense(config.neurons, input_dim=1, activation='relu'))
-        model.add(keras.layers.Dense(
-            config.neurons, input_dim=1, activation='relu'))  # coment
-        model.add(keras.layers.Dropout(config.dropout_ratio))  # comment
-        model.add(keras.layers.Dense(
-            1, bias_initializer=output_bias, activation='sigmoid'))
-        if output_bias is None:
-            model.layers[-1].bias.assign([0.0])
-
-        opt = tf.keras.optimizers.Adam(learning_rate=config.lr)
-
-
-
-        checkpoint_cb = keras.callbacks.ModelCheckpoint(os.path.join(model_loc, '{}_{}_{}_{}_{}.h5'.format(*nc)),
-                                                        save_best_only=True)
-
-        if weighted_binary==True:
-            model.compile(loss=custom_loss(weight_ratio),
-                        optimizer=opt, metrics=['accuracy'])
-
-        else:
-            model.compile(loss=weighted_binary,
-                        optimizer=opt, metrics=['accuracy'])
-
-
-        early_stopping = tf.keras.callbacks.EarlyStopping(
-                        monitor=hyparams['networks']['binary_classifier']['mointor'],
-                        min_delta=hyparams['networks']['binary_classifier']['min_delta'],
-                        patience=hyparams['networks']['binary_classifier']['patience'],
-                        restore_best_weights=True)
-
-        if not hyparams['networks']['binary_classifier']['wandb']:
-            bm_history = model.fit( train_bm,
-                                    validation_data=val_bm,
-                                    epochs=epochs
-                                    )        
-
-        elif save_model and hyparams['networks']['binary_classifier']['early_stopping']:
-            bm_history = model.fit( train_bm,
-                                    validation_data=val_bm,
-                                    epochs=epochs,
-                                    callbacks = [WandbCallback(), early_stopping, checkpoint_cb]
-                                    )           
-
-        elif hyparams['networks']['binary_classifier']['early_stopping']:
-            bm_history = model.fit( train_bm,
-                                    validation_data=val_bm,
-                                    epochs=epochs,
-                                    callbacks = [WandbCallback(), early_stopping]                                
-                                    )           
-
-        else:
-            bm_history = model.fit( train_bm,
-                                    validation_data=val_bm,
-                                    epochs=epochs,
-                                    callbacks = [WandbCallback()]
-                                    )           
-
-        
-        return bm_history, model
-
-
-# def loss(y_true, y_pred):
-#     when_y_1 = y_true*tf.keras.backend.log(y_pred)*(1/weight_ratio)
-#     neg_y_pred = Lambda(lambda x: -x)(y_pred)
-#     when_y_0 = ( 1+Lambda(lambda x: -x)(y_true))*tf.keras.backend.log(1+neg_y_pred )
-
-#     weighted_cross_entr = Lambda(lambda x: -x)(when_y_0+when_y_1)
-#     return weighted_cross_entr
+    max1 = traindict['x_ppl_box'].max() if traindict['y_ppl_box'].max() <= traindict['x_ppl_box'].max() else traindict['y_ppl_box'].max()
+    min1 = traindict['x_ppl_box'].min() if traindict['y_ppl_box'].min() >= traindict['x_ppl_box'].min() else traindict['y_ppl_box'].min()
+    lstm_model = lstm_train(traindict, max1, min1)
