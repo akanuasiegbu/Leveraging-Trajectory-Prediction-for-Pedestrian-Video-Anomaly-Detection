ab9f2449de4ef1647f9c4e34d5ec02027df2c9b4
Asiegbu Kanu-Asiegbu <akanu@wailord.engin.umich.edu>
2022-07-06 22:23:53 -0400

    moved find ranges function

--- custom_functions/utils.py
+++ custom_functions/utils.py
@@ -3,6 +3,18 @@ from os.path import join
 import numpy as np
 from config import hyparams, loc, exp
 from sklearn.metrics import roc_curve, auc
+import more_itertools as mit
+
+def find_ranges(iterable):
+    """Yield range of consecutive numbers."""
+    for group in mit.consecutive_groups(iterable):
+        group = list(group)
+        if len(group) == 1:
+            yield group[0]
+        else:
+            yield group[0], group[-1]
+
+
 
 def make_dir(dir_list):
     try:
